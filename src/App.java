4d6a00a350424a970d1da12d0d425d65a1cad16b
WillianFarias <williansousafarias@gmail.com>
2021-01-30 19:38:23 -0300

    ordenando lista e alterando valores

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Willian");
+        nomes.add("Pedro");
+        nomes.add("Juliana");
+        nomes.add("Carlos");
+        nomes.add("Maria");
+
+        System.out.println(nomes);
+        //ordenando lista de nomes
+        Collections.sort(nomes);
+
+        //Lista ordenada
+        System.out.println(nomes);
+
+        //Alterando conteudo da lista na posicao dois
+        nomes.set(2, "Larrisa");
+
+
     }
 }
