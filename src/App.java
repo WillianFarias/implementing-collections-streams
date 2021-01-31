d2c960b42fe7d0f844d9abc5322d5e31fd88fe10
WillianFarias <williansousafarias@gmail.com>
2021-01-30 21:39:26 -0300

    trabalhando com pilhas em java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -40,11 +43,50 @@ public class App {
         //Verifica se lista é vazia
         boolean vazia = nomes.isEmpty();
 
+        Iterator<String> iterator = nomes.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println("*** " + iterator.next());
+        }
+
         //limpa lista
         nomes.clear();
 
         //Veifica se o item existe na lista e retorna sua posicao
         int posicaoDoElemento = nomes.indexOf("Maria");
 
+        //Queue a forma como java trata filas
+        Queue<String> filaBanco = new LinkedList<>();
+
+        filaBanco.add("Patrícia");
+        filaBanco.add("Roberto");
+        filaBanco.add("Willian");
+        filaBanco.add("Maria");
+        filaBanco.add("João");
+
+        System.out.println(filaBanco);
+
+        //Retira o primeiro elemento da fila
+        String clienteASerAtendido = filaBanco.poll();
+
+        System.out.println(clienteASerAtendido);
+
+        System.out.println(filaBanco);
+
+        //retorna primeiro elemento da fila, mas nao retira ele da fila, caso a
+        //fila esteja vazia retorna null
+        String primeiroElemento = filaBanco.peek();
+
+        //retorna o primeiro elemento, caso esteja vazia retorna erro
+        String primeiroElementoOuErro = filaBanco.element();
+        //filaBanco.clear();
+        System.out.println(primeiroElementoOuErro);
+
+        Iterator<String> iteratorFila = filaBanco.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println("Iterator Fila: " + iteratorFila.next());
+        }
+        
     }
 }
