752b2f460781e5670e2bfbfa25a2b4c7d61073c7
WillianFarias <williansousafarias@gmail.com>
2021-02-04 09:17:25 -0300

    implementando comparacao usando lambda

---
+++ src/ComparatorExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComparatorExample {
+  public static void main(String[] args) {
+    List<Estudante> estudantes = new ArrayList<>();
+
+    estudantes.add(new Estudante("Willian", 29));
+    estudantes.add(new Estudante("Samanta", 25));
+    estudantes.add(new Estudante("Maria", 23));
+
+    System.out.println(estudantes);
+
+    estudantes.sort((first, second) -> first.getAge() - second.getAge());
+
+    System.out.println(estudantes);
+  }
+}
